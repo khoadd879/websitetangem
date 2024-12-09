9b018b29c33c6a70ef705048defb5ebe78b3a038
khoadd879 <163757386+khoadd879@users.noreply.github.com>
2024-12-10 01:50:16 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Mỹ à!',
+    introDesc: `Đây là code anh tặng cho em`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Thế em có chấp nhận là người yêu anh không ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, em không muốn :3',
+    question:'Tại sao em lại yêu anh ',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Còn một món quà cuối cùng nữa! Coi hong :3',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://khoadd879.github.io/hello/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
